5878435766c5def9db72a685867ed4a8c856c8b9
yash7xm <yash7xm@gmail.com>
2023-04-06 12:12:23 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,40 @@
+const givenCode = document.getElementById('message');
+const dropDown = document.getElementById('options');
+const button = document.getElementById('convert-btn');
+const res = document.getElementById('result');
+const loading = document.getElementById('loading');
+let langToBeConverted = dropDown.value;
+
+dropDown.addEventListener('change', () => {
+     langToBeConverted = dropDown.value;
+})
+res.readOnly = true;
+button.addEventListener('click', () => {
+    loading.style.display = 'block';
+  
+    if(!givenCode.value){
+        res.value = 'type something mf'
+        loading.style.display = 'none';
+        return;
+    }
+    console.log('hi');
+    fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            lang: langToBeConverted,
+            message: givenCode.value
+        })
+    })
+    .then(res => res.json())
+    .then(data => {
+        res.textContent = `${data.completion.content}`
+        loading.style.display = 'none';
+    })
+    .catch(error => {
+        res.textContent = `Some error has Occured on the server side please try again`
+        loading.style.display = 'none';
+    })
+})
